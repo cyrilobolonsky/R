8ca02fd8273e486c82bfa3fe2caa448cb0330ee8
Finance Code <cyrilobolonsky@gmail.com>
2022-03-26 22:07:40 +0100

    Update data_manipulation.R

--- data_manipulation.R
+++ data_manipulation.R
@@ -8,22 +8,24 @@ cash_flow <- c(1000, 4000, 550, 1500, 1100, 750, 6000)
 year <- c(1, 3, 4, 1, 2, 4, 5)
 
 #dataframe
-cash <- data.frame(company, cash_flow, year)
-cash
+data <- data.frame(company, cash_flow, year)
+data<-edit(data)  
+
+str(data)
+table(data)
+summary(data)
+attributes(data)
 
 #cbind
 apple <- c(109.49, 109.90, 109.11, 109.95, 111.03)
 ibm <- c(159.82, 160.02, 159.84, 160.35, 164.79)
 cor_matrix <- cor(cbind(apple, ibm))
 
-# Create a list
-portfolio <- list(name, apple, ibm, cor_matrix)
-
-str(cash)
-summary(cash)
-attributes(cash)
-
-attach(cash)
+data[3,2]
+data[5,"year"]
+data[12,]
+data[,2]
+data[7,2:4]
 
 cash$year
 cash$cash_flow*2
@@ -32,9 +34,6 @@ cash$double_year <- cash$year*2
 
 # Delete the company column
 cash$company <- NULL
-
-cash[3,2]
-cash[5,"year"]
 
 # Call head() for the first 4 rows
 head(cash, n = 4)
@@ -50,4 +49,5 @@ rownames(cash) <- c("company", "cash_flow", "year")
 subset(cash, company == 'B')
 subset(cash, year == 1)
 
-
+# Create a list
+portfolio <- list(name, apple, ibm, cor_matrix)
